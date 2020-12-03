67decc086e01d8495b8d589c5f28d63b6bf19546
Linus <56581587+linus-k519@users.noreply.github.com>
2020-12-03 14:37:11 +0100

    Create day02.js

---
+++ day02.js
@@ -0,0 +1,12 @@
+console.log(document.documentElement.innerText
+	.split("\n")
+  .filter(line => line != "")
+	.map(line => {
+		const [, min, max, letter, word] = line.match(/^([0-9]+)-([0-9]+)\s(\D):\s(\D*)/) || []
+		return {min, max, letter, word}
+	})
+	.reduce((acc, cur) => {
+		amount = (cur.word.match(new RegExp(cur.letter, "g")) || []).length
+		return acc + (cur.min <= amount && amount <= cur.max ? true : false)
+	}, 0)
+)
