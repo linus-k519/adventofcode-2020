57e0b0320cf1eec59a94506556045fe42a76b702
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2020-12-07 15:00:49 +0100

    Cid is optional

--- 2020/04/advent_2020_04_1.js
+++ 2020/04/advent_2020_04_1.js
@@ -1,8 +1,7 @@
 document.documentElement.innerText
   .split("\n\n")
-  .filter(line => line != "")
   .map(p => p.replaceAll("\n", " "))
   .map(p => p.split(" "))
-  .map(p => Object.fromEntries((p.map(kvs => kvs.split(":")))))
-  .filter(p => p.byr && p.iyr && p.eyr && p.hgt && p.hcl && p.ecl && p.pid && p.cid)
+  .map(p => Object.fromEntries(p.map(kv => kv.split(":"))))
+  .filter(p => p.byr && p.iyr && p.eyr && p.hgt && p.hcl && p.ecl && p.pid)
   .length
