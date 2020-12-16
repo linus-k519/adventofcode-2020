5c82d73cf7141ae39d24e5080cb06bbd59a6b90e
linus-k519 <56581587+linus-k519@users.noreply.github.com>
2020-12-16 12:57:46 +0100

    Day 14

---
+++ 2020/14/advent_2020_14_1.js
@@ -0,0 +1,41 @@
+class Mask {
+  constructor(mask) {
+    // Extract positive mask with all forced ones
+    this.pos = BigInt('0b' + mask.replaceAll("X", "0"))
+    // ... and negative mask with all forced zeros
+    this.neg = BigInt('0b' + mask.replaceAll("X", "1"))
+  }
+
+  apply(value) {
+    // Apply positve mask (forced ones)
+    // and negative mask (forced zeros) to value
+    value |= this.pos
+    value &= this.neg
+    return value
+  }
+}
+
+function update_mask(maskMatch) {
+  mask = new Mask(maskMatch[1])
+}
+
+mem = {}
+function update_mem(memMatch) {
+  // Parse memory positon and value
+  pos = parseInt(memMatch[1])
+  val = BigInt(memMatch[2])
+  mem[pos] = mask.apply(val)
+}
+
+// Parse document
+document.documentElement.innerText.split("\n")
+.forEach(line => {
+  maskMatch = line.match(/mask = ([X01]*)/)
+  memMatch = line.match(/mem\[(\d+)\] = (\d+)/)
+  if (maskMatch) update_mask(maskMatch)
+  else if (memMatch) update_mem(memMatch)
+});
+
+// Calculating sum
+sum = Object.values(mem).reduce((acc, cur) => acc + cur)
+console.log(`Sum ${sum}`)
