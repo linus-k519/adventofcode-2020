b86692839adf88b8b1dd04452ded6b806d31d8f3
linuskmr <linuskmr.dev@gmail.com>
2021-12-02 09:48:03 +0100

    Add 2021_02_task1

---
+++ 2021/02/advent_2021_02_task1.php
@@ -0,0 +1,29 @@
+
+<?php
+// Read input file
+$lines = file("/usr/src/app/input.txt");
+
+$depth = 0;
+$horizontal = 0;
+foreach ($lines as $line) {
+  // Remove \nb from line
+  $line = rtrim($line);
+  // Split line on whitespace
+  $line = explode(' ', $line);
+  switch ($line[0]) {
+    case "forward":
+      $horizontal += $line[1];
+      break;
+    case "down":
+      $depth += $line[1];
+      break;
+    case "up":
+      $depth -= $line[1];
+      break;
+  }
+}
+
+// echo "horizontal " . $horizontal . "\n";
+// echo "depth " . $depth . "\n";
+echo "result " . ($horizontal * $depth) . "\n";
+?>
