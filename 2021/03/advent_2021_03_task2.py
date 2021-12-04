128aa525d4bcf35f0da2044f6c240334ca19468e
linuskmr <linuskmr.dev@gmail.com>
2021-12-04 14:18:33 +0100

    Progress

--- 2021/03/advent_2021_03_task2.py
+++ 2021/03/advent_2021_03_task2.py
@@ -1,3 +1,4 @@
+import copy
 from typing import List
 
 import numpy as np
@@ -14,28 +15,57 @@ input_data = [list(line) for line in input_data]
 # Convert each character ('0' or '1') to an int (0 or 1)
 input_data = [[int(n) for n in row] for row in input_data]
 # Create numpy array and transpose it, so that rows are no columns and vice versa
-input_data = np.array(input_data)
-input_data = input_data.transpose()
+input_data_np = np.array(input_data)
+input_data_transposed = input_data_np.transpose()
 
-gamma_rate = 0
-epsilon_rate = 0
+most_common_bits = []
+least_common_bits = []
+
 
 # Because the ndarray is now transposed, the numbers for gamma and epsilon rate are simply the rows
-for row in input_data:
+for row in input_data_transposed:
     # Convert numpy array to list...
     row = row.tolist()
     # ... to be able to find the most and least common elements
-    most_common = max(set(row), key=row.count)
-    least_common = min(set(row), key=row.count)
+    count0 = row.count(0)
+    count1 = row.count(1)
+    print(f'{count0=} {count1=}')
 
-    # 'Add' the current bit as the least significant bit
-    gamma_rate <<= 1
-    gamma_rate |= most_common
+    # If 0 and 1 are equally common, keep values with a 1 in the position being considered.
+    most_common = 1 if count1 >= count0 else 0
+    # If 0 and 1 are equally common, keep values with a 0 in the position being considered.
+    least_common = 0 if count0 >= count1 else 1
 
-    epsilon_rate <<= 1
-    epsilon_rate |= least_common
+    # 'Add' the current bit
+    most_common_bits.append(most_common)
+    least_common_bits.append(least_common)
 
-# print(f'{gamma_rate=} {epsilon_rate=}')
+print('most common bits', most_common_bits)
 
-result = gamma_rate * epsilon_rate
+
+def calculate_rating(rating: List[List[int]], common_bits: List[int]) -> int:
+    for bit_position in range(len(input_data[0])):
+        if len(rating) == 1:
+            # If you only have one number left, stop
+            break
+        # Keep only numbers selected by the bit criteria for the type of rating value for which you are searching.
+        # Discard numbers which do not match the bit criteria.
+        common = common_bits[bit_position]
+        rating = list(filter(lambda number: number[bit_position] == common, rating))
+        print(f'{rating=}')
+
+    rating = rating[0]
+    rating = [str(x) for x in rating]
+    rating = int(''.join(rating), base=2)
+    return rating
+
+
+print('oxygen')
+oxygen_generator_rating = calculate_rating(copy.deepcopy(input_data), most_common_bits)
+print('co2')
+co2_scrubber_rating = calculate_rating(copy.deepcopy(input_data), least_common_bits)
+
+print(f'{oxygen_generator_rating=} {co2_scrubber_rating=}')
+
+result = oxygen_generator_rating * co2_scrubber_rating
 print(result)
